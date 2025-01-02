ba8460bd150df3c6f7c987a0625ec73349c7e9e9
suehtamxx <eldermatheus60@gmail.com>
2025-01-02 18:15:44 -0300

    Co-authored-by: VivySilva <VivySilva@users.noreply.github.com>

--- Arvore Rubro Negra/rubronegra.c
+++ Arvore Rubro Negra/rubronegra.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "rubronegra.h"
 
-int main()
+#define int 1 = black;
+#define int 2 = red;
+
+void inserir_arvRN(arv_ptbr **R, arv_ptbr *no)
 {
-    printf("oi!");
+    if(*R == NULL)
+        *R = no;
+    else{
+        if(strcmp(no->info.ptbr, (*R)->info.ptbr) < 0)
+            inserir_arvRN(&((*R)->esq), no);
+        else inserir_arvRN(&((*R)->dir), no);
+
+    if(cor((*R)->esq) == 1 && cor((*R)->dir) == 2)
+        rotacionaEsq(R);
+    if(cor((*R)->esq) == 2 && cor((*R)->esq->esq) == 2)
+        rotacionaDir(R);
+    if(cor((*R)->esq) == 2 && cor((*R)->dir) == 2)
+        trocarCor(R);
+    }
 }
--- Arvore Rubro Negra/rubronegra.h
+++ Arvore Rubro Negra/rubronegra.h
@@ -1,3 +1,45 @@
 #ifndef RUBRONEGRA_H
 #define RUBRONEGRA_H
+
+#define black = 1;
+#define red = 2;
+
+typedef struct lista_unidade
+{
+    int unidade;
+    struct lista_unidade *prox;
+}unidade;
+
+typedef struct informacao_ingles
+{
+    char ingles[100];
+    unidade *l_unidade;
+}info_ingles;
+
+typedef struct arvore_ingles
+{
+    struct arvore_ingles *esq, *dir;
+    info_ingles info;
+}arv_ingles;
+
+typedef struct informacao_ptbr
+{
+    char ptbr[100];
+    arv_ingles *ingles;
+}info_ptbr;
+
+typedef struct arvore_portugues
+{
+    struct arvore_portugues *esq, *dir;
+    info_ptbr info;
+    int cor;
+}arv_ptbr;
+
+arv_ingles *criar_arvBB();
+arv_ptbr *criar_arvRN();
+
+arv_ingles *criar_no_arvBB();
+arv_ptbr *criar_no_arvRN();
+
+void inserir_arvRN(arv_ptbr **R, arv_ptbr *no);
 #endif

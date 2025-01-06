e360e199080a5d3e3f4c8f1fdb8b545a2684b7f7
suehtamxx <eldermatheus60@gmail.com>
2025-01-06 13:33:09 -0300

    Update rubronegra.h

--- Arvore Rubro Negra/rubronegra.h
+++ Arvore Rubro Negra/rubronegra.h
@@ -63,7 +63,7 @@ void funcaoauxI(arv_ptbr *no, int uni);
 void funcaoI(arv_ingles *no, int uni, char *palavra);
 void funcaoII(arv_ingles *no, char *palavra);
 void funcaoauxII(arv_ptbr *no, char *palavra);
-void funcaoIII(arv_ptbr **no, char *palavra, int uni);
+void funcaoIII(arv_ingles **no, char *palavra, int uni);
 void funcaoauxIII(arv_ptbr **no, arv_ptbr **portugues, char *palavra, int uni);
 void liberar_arv_BB(arv_ingles *no);
 void liberar_arvRN(arv_ptbr *no);

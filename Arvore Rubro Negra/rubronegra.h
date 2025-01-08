b710e3d2f3692c6aa0e37ac953ebfa6e75933f60
suehtamxx <eldermatheus60@gmail.com>
2025-01-07 22:40:16 -0300

    Update rubronegra.h

--- Arvore Rubro Negra/rubronegra.h
+++ Arvore Rubro Negra/rubronegra.h
@@ -37,13 +37,13 @@ typedef struct arvore_portugues
 
 unidade *criar_no_l_unid();
 arv_ingles *criar_no_arvBB();
-arv_ptbr *criar_no_arvRN();
+arv_ptbr *criar_no_arv_RN();
 
 int cor(arv_ptbr *no);
 void trocaCor(arv_ptbr *no);
 
 int inserir_arv_BB(arv_ingles **ingles, arv_ingles *no);
-void inserir_arvRN(arv_ptbr **R, arv_ptbr *no);
+void inserir_arv_RN(arv_ptbr **R, arv_ptbr *no);
 void move2EsqRed(arv_ptbr **r);
 void move2DirRed(arv_ptbr **r);
 arv_ptbr *balancear(arv_ptbr *r);
@@ -52,22 +52,23 @@ arv_ptbr *procuraMenor(arv_ptbr *no);
 
 
 arv_ptbr *remove_no(arv_ptbr *no, char *valor);
-int remove_arvRN(arv_ptbr **no, char *valor);
+int remove_arv_RN(arv_ptbr **no, char *valor);
 
 int remover_arv_BB(arv_ingles **ingles, arv_ingles *no);
 int eh_folha_BB(arv_ingles *ingles);
 arv_ingles *menor_filho_BB(arv_ingles *ingles);
 arv_ingles *so_um_filho_BB(arv_ingles *ingles);
 
-void funcaoauxI(arv_ptbr *no, int uni);
-void funcaoI(arv_ingles *no, int uni, char *palavra);
-void funcaoII(arv_ingles *no, char *palavra);
-void funcaoauxII(arv_ptbr *no, char *palavra);
-void funcaoIII(arv_ingles **no, char *palavra, int uni);
-void funcaoauxIII(arv_ptbr **no, arv_ptbr **portugues, char *palavra, int uni);
-void funcaoIV(arv_ptbr **no, arv_ptbr **portugues, char *palavra, int uni);
+void imprimir_unidade_RN(arv_ptbr *no, int uni);
+void imprimir_unidade_BB(arv_ingles *no, int uni, char *palavra);
+void imprimir_ptbr_BB(arv_ingles *no, char *palavra);
+void imprimir_ptbr_RN(arv_ptbr *no, char *palavra);
+void remover_ingles_BB(arv_ingles **no, char *palavra, int uni);
+void remover_ingles_RN(arv_ptbr **no, arv_ptbr **portugues, char *palavra, int uni);
+void remover_portugues_BB(arv_ingles **no, int uni);
+void remover_portgues_RN(arv_ptbr **no, arv_ptbr **portugues, char *palavra, int uni);
 
 void liberar_arv_BB(arv_ingles *no);
-void liberar_arvRN(arv_ptbr *no);
+void liberar_arv_RN(arv_ptbr *no);
 void liberar_lista_unidades(unidade *l_unidade);
 #endif

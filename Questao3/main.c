d4a18423204e98987d1831fc0dfa3f3c5b961eeb
suehtamxx <eldermatheus60@gmail.com>
2025-01-10 00:43:35 -0300

    Update main.c

--- Questao3/main.c
+++ Questao3/main.c
@@ -6,34 +6,71 @@
 
 #define Ocupado 0;
 #define Livre 1;
-arv_blocos *blocos;
-int qtd = 0;
-int *numeroF = 0;
+
 void imprimir_arvore_B3(arv_blocos *blocos) 
 {
     if (blocos != NULL)
     {
-        imprimir_arvore_B3(blocos->esq);
 
-        printf("Inicial: %d | Final: %d - %d\n", blocos->info1.blocoi, blocos->info1.blocof, blocos->info1.status);
+        if(blocos->esq != NULL)
+            imprimir_arvore_B3(blocos->esq);
+
+        printf("Inicial: %d | Final: %d - %d(INFO 1)\n", blocos->info1.blocoi, blocos->info1.blocof, blocos->info1.status);
+
+        imprimir_arvore_B3(blocos->cen);
 
         if (blocos->nInfos == 2)
         {
-            printf("Inicial: %d | Final: %d - %d\n", blocos->info2.blocoi, blocos->info2.blocof, blocos->info2.status);
+            printf("Inicial: %d | Final: %d - %d(INFO 2)\n", blocos->info2.blocoi, blocos->info2.blocof, blocos->info2.status);
             imprimir_arvore_B3(blocos->dir); 
         }
-        imprimir_arvore_B3(blocos->cen);
     }
 }
 int main()
 {
+    arv_blocos *blocos;
+    blocos = NULL;
+    int flag = 0, qtd = 0, op = 0;
+    
     do{
         printf("Informe a quantidade de blocos a serem alocados(maior que 0):\n");
         scanf(" %d", &qtd);
         if(qtd <= 0)printf("Valor inválido. Informe um valor maior que 0\n");
     }while(qtd <= 0);
 
+    
     cadastrar(&blocos, qtd);
+    imprimir_arvore_B3(blocos);
+    
+    do{
+
+
+        printf("\n----------MENU----------\n");
+        printf("1 - Alocar No \n");
+        printf("2 - Desalocar No \n");
+        printf("0 - Sair \n");
+        printf("Digite a opcao:\n");
+        scanf(" %d", &op);
+
+        switch (op)
+        {
+        case 1:
+            printf("Informe a quantidade de nós para alocar:\n");
+            scanf(" %d", &qtd);
+             ocupa_espaco(&blocos, &blocos, &qtd, &flag);
+            
+            break;
+        case 2:
+            printf("Informe a quantiade de nos para desalocar:\n");
+            scanf(" %d", &qtd);
+            //no = buscar_no(blocos, 0, qtd);
+            
+            break;
+        default:
+            printf("Tchau...\n");
+            break;
+        }
+    }while(op != 0);
 
     imprimir_arvore_B3(blocos);
     
